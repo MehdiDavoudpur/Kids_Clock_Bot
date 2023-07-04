7ff9eb5a78681a20129fcfb9b3c70f633ea2738d
Mehdi <davoudpour2000@gmail.com>
2023-07-04 16:52:49 +0330

    add temp

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import telebot
 import datetime
-import streamlit as st
+import requests
 
+bot = telebot.TeleBot("6315993694:AAFqBhqI7ROH_MmD6JxDNGAcAXWqf66XfY4")
 
-bot = telebot.TeleBot("6171477907:AAEna8FFur3jzZheD6AIBgBZFRNTu8qMUeM")
-
-st.title("i'm a web for kidsClock tel Bot")
 print("hello")
 
 
-@bot.message_handler(commands=['times'])
-def send_welcome(message):
+@bot.message_handler(commands=['time'])
+def send_message(message):
     bot.reply_to(message, time_message())
+
+
+@bot.message_handler(commands=['temp'])
+def send_message(message):
+    bot.reply_to(message, temp_message())
 
 
 def time_message():
@@ -37,7 +40,39 @@ def time_message():
     Auckland_Date = Auckland_time.strftime('%A')
     Auckland_Time = Auckland_time.strftime('%H:%M:%S')
 
-    message = "San Jose" + "\n" + SanJose_Date + "\n" + SanJose_Time + "\n\n" + "Belfast" + "\n" + Belfast_Date + "\n" + Belfast_Time + "\n\n" + "Rotterdam" + "\n" + Rotterdam_Date + "\n" + Rotterdam_Time + "\n\n" + "Tehran" + "\n" + Tehran_Date + "\n" + Tehran_Time + "\n\n" + "Auckland" + "\n" + Auckland_Date + "\n" + Auckland_Time
+    message = "San Jose:".upper() + "\n" + SanJose_Date + "\n" + SanJose_Time + "\n\n" \
+              + "Belfast:".upper() + "\n" + Belfast_Date + "\n" + Belfast_Time + "\n\n" \
+              + "Rotterdam:".upper() + "\n" + Rotterdam_Date + "\n" + Rotterdam_Time + "\n" + "\n\n" \
+              + "Tehran:".upper() + "\n" + Tehran_Date + "\n" + Tehran_Time + "\n" + "\n\n" \
+              + "Auckland:".upper() + "\n" + Auckland_Date + "\n" + Auckland_Time
+
+    return message
+
+
+def temp_message():
+    san_jose_url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/san%20jose?unitGroup=metric&elements=datetimeEpoch%2Ctemp&include=current&key=CB93XCP3CPJN2SRCDKYZCP5TU&contentType=json"
+    belfast_url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/belfast?unitGroup=metric&elements=datetimeEpoch%2Ctemp&include=current&key=CB93XCP3CPJN2SRCDKYZCP5TU&contentType=json"
+    rotterdam_url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/rotterdam?unitGroup=metric&elements=datetimeEpoch%2Ctemp&include=current&key=CB93XCP3CPJN2SRCDKYZCP5TU&contentType=json"
+    tehran_url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/tehran?unitGroup=metric&elements=datetimeEpoch%2Ctemp&include=current&key=CB93XCP3CPJN2SRCDKYZCP5TU&contentType=json"
+    auckland_url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/auckland?unitGroup=metric&elements=datetimeEpoch%2Ctemp&include=current&key=CB93XCP3CPJN2SRCDKYZCP5TU&contentType=json"
+
+    san_jose_data = requests.get(san_jose_url).json()
+    belfast_data = requests.get(belfast_url).json()
+    rotterdam_data = requests.get(rotterdam_url).json()
+    tehran_data = requests.get(tehran_url).json()
+    auckland_data = requests.get(auckland_url).json()
+
+    san_jose_temp = str(san_jose_data["currentConditions"]["temp"])
+    belfast_temp = str(belfast_data["currentConditions"]["temp"])
+    rotterdam_temp = str(rotterdam_data["currentConditions"]["temp"])
+    tehran_temp = str(tehran_data["currentConditions"]["temp"])
+    auckland_temp = str(auckland_data["currentConditions"]["temp"])
+
+    message = "San Jose:".upper() + "\n" + san_jose_temp + "\u00b0C" + "\n\n" \
+              + "Belfast:".upper() + "\n" + belfast_temp + "\u00b0C" + "\n\n" \
+              + "Rotterdam:".upper() + "\n" + rotterdam_temp + "\u00b0C" + "\n\n" \
+              + "Tehran:".upper() + "\n" + tehran_temp + "\u00b0C" + "\n\n" \
+              + "Auckland:".upper() + "\n" + auckland_temp + "\u00b0C"
 
     return message
 
